786da348d4038acf488dddaf4cafbb158eed63e2
Fabi <frontfabi@gmail.com>
2020-09-23 21:45:29 -0300

    [FIX] refreshing gatsby and cache

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/fabiana/Projetos/Fabi/perifa/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/fabiana/Projetos/Fabi/perifa/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/fabiana/Projetos/Fabi/perifa/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/home/fabiana/Projetos/Fabi/perifa/src/pages/page-2.js"))),
