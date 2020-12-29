35449b028218b30015e497a657c58610694d505f
Fabiana Rodrigues <45862435+frontfabi@users.noreply.github.com>
2020-12-29 20:52:00 -0300

    [ADD] slider

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,35 @@
 import React from "react"
-
+import Carousel from 'nuka-carousel';
 import StyledHero from "./Hero.style"
+
+const content = [{
+  image: 'https://loremflickr.com/640/360',
+  title: 'Já somos quase 1000 no nosso Discord!',
+  button: 'Inscreva-se'
+},
+{
+  image: 'https://loremflickr.com/640/360',
+  title: 'Nossos plantões estão a todo vapor!',
+  button: 'Entenda os plantões'
+}]
 
 const Hero = () => {
   return (
     <StyledHero>
-      <main className="hero-image">
-        <section className="hero-text">
-          <h1>Já somos quase 1000 no nosso Discord</h1>
-          <a href="/">Inscreva-se</a>
-        </section>
-      </main>
+      <Carousel wrapAround slidesToShow={1} cellAlign="center">
+        {content.map((item, index) => (
+          <div
+            key={index}
+            className="hero-image"
+            style={{ background: `url('${item.image}') no-repeat center center`, backgroundSize: 'cover' }}
+          >
+            <div className="center hero-text">
+              <h1>{item.title}</h1>
+              <a href="/">{item.button}</a>
+            </div>
+          </div>
+        ))}
+      </Carousel>
     </StyledHero>
   )
 }
