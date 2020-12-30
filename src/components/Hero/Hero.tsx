14c00782ce5955f2e9f4c4fe4018a8c65bf5f3f0
Fabiana Rodrigues <45862435+frontfabi@users.noreply.github.com>
2020-12-29 21:18:32 -0300

    [fix]

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,24 +6,12 @@ const content = [{
   image: 'https://loremflickr.com/640/360',
   title: 'Já somos quase 1000 no nosso Discord!',
   button: 'Inscreva-se'
-<<<<<<< HEAD
-=======
-},
-{
-  image: 'https://loremflickr.com/640/360',
-  title: 'Nossos plantões estão a todo vapor!',
-  button: 'Entenda os plantões'
->>>>>>> 798583205f9fdd6d3a3ec09c7375643fe0a321e1
 }]
 
 const Hero = () => {
   return (
     <StyledHero>
-<<<<<<< HEAD
       <Carousel wrapAround autoplay slidesToShow={1} cellAlign="center">
-=======
-      <Carousel wrapAround slidesToShow={1} cellAlign="center">
->>>>>>> 798583205f9fdd6d3a3ec09c7375643fe0a321e1
         {content.map((item, index) => (
           <div
             key={index}
@@ -33,11 +21,7 @@ const Hero = () => {
             <div className="center hero-text">
               <h1>{item.title}</h1>
               <a href="/">{item.button}</a>
-<<<<<<< HEAD
             </div> 
-=======
-            </div>
->>>>>>> 798583205f9fdd6d3a3ec09c7375643fe0a321e1
           </div>
         ))}
       </Carousel>
