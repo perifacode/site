18cd941c92776fab7164b6f2b5f3c8359a802af6
Guilherme Vieira <glvs011@gmail.com>
2021-08-11 23:12:43 -0300

    feat: create files to recreate components in vue

---
+++ components/atoms/Anchor.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    props: {
+      href: {
+        type: String,
+        required: false
+      },
+      to: {
+        type: String,
+        required: false
+      }
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+.anchor-wrapper {
+    position: relative;
+    display: block;
+    text-align: center;
+}
+</style>
