fd73689ab7e7f8ee279a532a0e852a081d4c675f
Guilherme Vieira <glvs011@gmail.com>
2021-08-11 22:46:44 -0300

    feat: add nuxt content

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,6 +44,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxt/content'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
