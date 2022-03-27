d90095bf431ea2627d9394df7e85fc08f711b5ea
= <=>
2022-03-26 21:53:35 -0300

    feat: finish about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,16 +1,27 @@
-import { Flex, Heading } from "@chakra-ui/react"
+import { Flex, Image, Text } from "@chakra-ui/react"
 
 export const AboutSection = () => {
   return (
     <Flex
       bg="url('https://media.graphcms.com/BVOJKWdQQ0WJ2p2y76gG?_ga=2.139763194.899926076.1648319869-549734489.1647827264')"
       bgColor="blackAlpha.900"
-      h="50vh"
+      h="60vh"
       justify="center"
       align="center"
     >
-      <Flex bg="whiteAlpha.900" p="3rem">
-        <Heading as="h2">Sobre</Heading>
+      <Flex 
+        bg="white" 
+        p="3rem" 
+        direction="column" 
+        maxWidth="800px" 
+        py="2rem"
+      >
+        <Image src="/sobre-grafite.svg" alt="Sobre" w="200px" />
+        <Text>
+        A Perifacode é uma ONG que tem com objetivo “desgourmetizar” o mercado digital, preenchido, em sua maioria, por um estereótipo ao qual precisa ser mudado para ser mais inclusivo e com esse propósito a ONG foi criada em 2019 para capacitar, discutir e fazer ouvir a voz da periferia dentro do mercado digital. 
+
+        Realizamos diversos eventos presenciais para capacitar esses jovens da periferia e contavam com vários grupos espalhados pelas redes sociais, contamos com uma comunidade ativa no Discord para discutirmos sobre programação e temas relacionados.
+        </Text>
       </Flex>
     </Flex>
   )
