93793240227e9efb99d1ab1287b6c2c870bbbd39
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-21 23:26:16 -0300

    add alt in img logo perifaCode

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,6 +8,7 @@ export const Logo = () => {
         src="https://media.graphcms.com/OdWKElwPSEia9nfabuxw?_ga=2.34469153.1858804931.1648141850-710560506.1647625745" 
         width={150}
         cursor="pointer"
+        alt="Logo PerifaCode"
       />
     </Link>
   )
