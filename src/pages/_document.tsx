63ac3d32828e2327f2c844a1c61f054f4c31977e
Lucas <lucasferreiralimax@gmail.com>
2022-10-22 10:13:05 -0300

    Improvement (#55)
    * set lang pt-BR in document HTML
    
    * add alt in img logo perifaCode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document"
 
 const Document = () => {
   return (
-    <Html>
+    <Html lang="pt-BR">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
